903874f3786dfeb7fc2a82276a613e80cd420cae
jsk56143 <47489757+jsk56143@users.noreply.github.com>
2022-07-20 10:53:58 -0400

    Update weekly-reminder.js

--- weekly-reminder.js
+++ weekly-reminder.js
@@ -26,16 +26,60 @@ const SHEET_ROWS = {
   YOUTH_ASSISTANTS: "Youth Assistants"
 }
 
+/*
+// Enums for all users of this script
+const USERS = {
+  JEVONS: "Jevons"
+}
+var userArray = Object.values(USERS);
+*/
+const user = "Josh"
+//const userID = "U03P9EMLB98" // memberID in Slack
+
 const spreadsheet = SpreadsheetApp.openById("1TlPboL6wBnEKFkOQW0rii0aBevuGK4PW0bFW1a8kqA0");
 const logistics_sheet = spreadsheet.getSheetByName("Logistics");
 let rotation_fieldCount = 12; // Update this if you add more fields to the rotation object
 
-function weeklyRotations() {
-  let postMessage_payload = formatPayload();
+/*
+function sendWeeklyRotations() {
+  for (let i = 0; i < userArray.length; i++) {
+    let postMessage_payload = formatPayload(userArray[i]);
+    let post_message_request = requestSlack("POST", "chat.postMessage", postMessage_payload);
+    Logger.log("POST message request: " + post_message_request);
+    rest();
+  }
+}
+
+function updateHourlyRotations() {
+  for (let i = 0; i < userArray.length; i++) {
+    let postMessage_payload = formatPayload(userArray[i]);
+    let post_message_request = requestSlack("POST", "chat.update", postMessage_payload);
+    Logger.log("POST message request: " + post_message_request);
+    rest();
+  }
+}
+*/
+
+function sendWeeklyRotations() {
+  let postMessage_payload = formatPayload_postMessage(user);
   let post_message_request = requestSlack("POST", "chat.postMessage", postMessage_payload);
-  console.log("POST message request: " + post_message_request);
+  console.log("AAAAA: " + post_message_request.response_data.ts);
+  Logger.log("Post message request: " + JSON.stringify(post_message_request));
+  saveTimestamp(post_message_request.response_data.ts);
+  saveChannelID(post_message_request.response_data.channel);
+}
+
+function updateHourlyRotations() {
+  let ts = readTimestamp();
+  let channelID = readChannelID();
+  let updateMessage_payload = formatPayload_updatePost(user, ts, channelID);
+  let update_message_request = requestSlack("POST", "chat.update", updateMessage_payload);
+  Logger.log("Update message request: " + JSON.stringify(update_message_request));
+  saveTimestamp(update_message_request.response_data.ts)
   rest();
 }
+
+
 
 function getLatestRotations() {
   let startingCol = parseInt(readStartingSheetColumn());
@@ -43,9 +87,9 @@ function getLatestRotations() {
     console.log("Key-value pair data not saved. Check the Script Properties in the Settings.")
   }
   console.log("Starting col: " + startingCol);
-  //saveStartingSheetColumn(startingCol + 2);
+  saveStartingSheetColumn(startingCol + 2);
 
-  let values = logistics_sheet.getRange(1,startingCol,20,2).getValues(); // getRange(starting row index, starting column index, num. of rows to return, num. of cols to return)
+  let values = logistics_sheet.getRange(1,startingCol,20,4).getValues(); // getRange(starting row index, starting column index, num. of rows to return, num. of cols to return)
   //console.log(values);
 
   let rows = getRows();
@@ -80,6 +124,36 @@ function getLatestRotations() {
       help: values[rows.help][1],
       training: values[rows.training][1],
       youthAssistants: values[rows.youthAssistants][1]
+    },
+    third: {
+      day: getFormattedDay(values[rows.day][2]),
+      date: getFormattedDate(values[3][2]),
+      rotationStatus: values[rows.rotationStatus][2],
+      prayerLead: values[rows.prayerLead][2],
+      vidDirector: values[rows.vidDirector][2],
+      proPres: values[rows.proPres][2],
+      cam1OP: values[rows.cam1OP][2],
+      cam2OP: values[rows.cam2OP][2],
+      cam3OP: values[rows.cam3OP][2],
+      editor: values[rows.editor][2],
+      help: values[rows.help][2],
+      training: values[rows.training][2],
+      youthAssistants: values[rows.youthAssistants][2]
+    },
+    fourth: {
+      day: getFormattedDay(values[rows.day][3]),
+      date: getFormattedDate(values[3][3]),
+      rotationStatus: values[rows.rotationStatus][3],
+      prayerLead: values[rows.prayerLead][3],
+      vidDirector: values[rows.vidDirector][3],
+      proPres: values[rows.proPres][3],
+      cam1OP: values[rows.cam1OP][3],
+      cam2OP: values[rows.cam2OP][3],
+      cam3OP: values[rows.cam3OP][3],
+      editor: values[rows.editor][3],
+      help: values[rows.help][3],
+      training: values[rows.training][3],
+      youthAssistants: values[rows.youthAssistants][3]
     }
   }
   //console.log(rotations);
@@ -147,25 +221,74 @@ function getRows() {
   return rows;
 }
 
-function saveStartingSheetColumn() {
+function saveStartingSheetColumn(startingCol) {
   try {
     const userProperties = PropertiesService.getUserProperties();
-    userProperties.setProperty('NEW_COL', 162);
+    userProperties.setProperty('NEW_COL', startingCol);
   } catch (err) {
-    Logger.log('Failed with error %s', err.message);
+    Logger.log('Failed saving starting sheet column: %s', err.message);
   }
 }
 
 function readStartingSheetColumn() {
   try {
-    // Get the value for the user property 'DISPLAY_UNITS'.
+    // Get the value for the user property 'NEW_COL'.
     const userProperties = PropertiesService.getUserProperties();
     const col = userProperties.getProperty('NEW_COL');
     console.log("col: " + col);
+    Logger.log("Read starting sheet column: "+ col);
     return col;
   } catch (err) {
     // TODO (developer) - Handle exception
-    console.log("col: " + col);
+    Logger.log('Failed reading starting sheet column: %s', err.message);
+    return -1;
+  }
+}
+
+function saveTimestamp(timestamp) {
+  try {
+    Logger.log("Save timestamp: " + timestamp);
+    const userProperties = PropertiesService.getUserProperties();
+    userProperties.setProperty('Josh_ts', timestamp);
+  } catch (err) {
+    Logger.log('Failed saving timestamp: %s', err.message);
+  }
+}
+
+function readTimestamp() {
+  try {
+    // Get the value for the user property 'Josh_ts'.
+    const userProperties = PropertiesService.getUserProperties();
+    const ts = userProperties.getProperty('Josh_ts');
+    Logger.log("Read Josh_timestamp: " + ts);
+    return ts;
+  } catch (err) {
+    // TODO (developer) - Handle exception
+    Logger.log('Failed reading Josh_timestamp: %s', err.message);
+    return -1;
+  }
+}
+
+function saveChannelID(channelID) {
+  try {
+    Logger.log("Save Josh_channelID: " + channelID);
+    const userProperties = PropertiesService.getUserProperties();
+    userProperties.setProperty('Josh_channelID', channelID);
+  } catch (err) {
+    Logger.log('Failed saving Josh_channelID: %s', err.message);
+  }
+}
+
+function readChannelID() {
+  try {
+    // Get the value for the user property 'Josh_channelID'.
+    const userProperties = PropertiesService.getUserProperties();
+    const channelID = userProperties.getProperty('Josh_channelID');
+    Logger.log("Read Josh_channelID: " + channelID);
+    return channelID;
+  } catch (err) {
+    // TODO (developer) - Handle exception
+    Logger.log('Failed reading Josh_channelID: %s', err.message);
     return -1;
   }
 }
@@ -185,14 +308,24 @@ function getFormattedDay(day) {
   } else if (day == "SUNDAY") {
     return "Sunday";
   } else {
-    return -1;
+    return "Not Friday/Sunday";
   }
 }
 
-function formatPayload() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function rest() {
+  console.log('Taking a break...');
+  await sleep(5000);
+  console.log('Five seconds later');
+}
+
+function formatPayload_postMessage(user) {
   let rotation = getLatestRotations();
   let payload = {
-    channel: "<>",
+    channel: "U03P9EMLB98",
     blocks: [
       {
         type: "section",
@@ -223,14 +356,14 @@ function formatPayload() {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `A`
+          text: ``
         }
       },
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `B`
+          text: ``
         }
       }
     ]
@@ -238,6 +371,8 @@ function formatPayload() {
 
   let first = `*${rotation.first.day} (${rotation.first.date})*: *_${rotation.first.rotationStatus}_* \n`;
   let second = `*${rotation.second.day} (${rotation.second.date})*: *_${rotation.second.rotationStatus}_* \n`;
+  let third = `*${rotation.third.day} (${rotation.third.date})*: *_${rotation.third.rotationStatus}_* \n`;
+  let fourth = `*${rotation.fourth.day} (${rotation.fourth.date})*: *_${rotation.fourth.rotationStatus}_* \n`;
 
   if (rotation.first.prayerLead) {
      first += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.first.prayerLead}* \n`;
@@ -301,27 +436,269 @@ function formatPayload() {
   if (rotation.second.youthAssistants) {
      second += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.second.youthAssistants}* \n`;
   }
+
+  if (rotation.third.prayerLead.includes(user, 0)) {
+     third += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.third.prayerLead}* \n`;
+  }
+  if (rotation.third.vidDirector.includes(user, 0)) {
+     third += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.third.vidDirector}* \n`;
+  }
+  if (rotation.third.proPres.includes(user, 0)) {
+     third += `>${SHEET_ROWS.PROPRES}: *${rotation.third.proPres}* \n`;
+  }
+  if (rotation.third.cam1OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM1OP}: *${rotation.third.cam1OP}* \n`;
+  }
+  if (rotation.third.cam2OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM2OP}: *${rotation.third.cam2OP}* \n`;
+  }
+  if (rotation.third.cam3OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM3OP}: *${rotation.third.cam3OP}* \n`;
+  }
+  if (rotation.third.editor.includes(user, 0)) {
+     third += `>${SHEET_ROWS.EDITOR}: *${rotation.third.editor}* \n`;
+  }
+  if (rotation.third.help.includes(user, 0)) {
+     third += `>${SHEET_ROWS.HELP}: *${rotation.third.help}* \n`;
+  }
+  if (rotation.third.training.includes(user, 0)) {
+     third += `>${SHEET_ROWS.TRAINING}: *${rotation.third.training}* \n`;
+  }
+  if (rotation.third.youthAssistants.includes(user, 0)) {
+     third += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.third.youthAssistants}* \n`;
+  }
+
+  if (rotation.fourth.prayerLead.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.fourth.prayerLead}* \n`;
+  }
+  if (rotation.fourth.vidDirector.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.fourth.vidDirector}* \n`;
+  }
+  if (rotation.fourth.proPres.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.PROPRES}: *${rotation.fourth.proPres}* \n`;
+  }
+  if (rotation.fourth.cam1OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM1OP}: *${rotation.fourth.cam1OP}* \n`;
+  }
+  if (rotation.fourth.cam2OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM2OP}: *${rotation.fourth.cam2OP}* \n`;
+  }
+  if (rotation.fourth.cam3OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM3OP}: *${rotation.fourth.cam3OP}* \n`;
+  }
+  if (rotation.fourth.editor.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.EDITOR}: *${rotation.fourth.editor}* \n`;
+  }
+  if (rotation.fourth.help.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.HELP}: *${rotation.fourth.help}* \n`;
+  }
+  if (rotation.fourth.training.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.TRAINING}: *${rotation.fourth.training}* \n`;
+  }
+  if (rotation.fourth.youthAssistants.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.fourth.youthAssistants}* \n`;
+  }
    
   payload.blocks[1].text.text = first;
   payload.blocks[2].text.text = second;
+  payload.blocks[4].text.text = third;
+  payload.blocks[5].text.text = fourth;
 
   return payload;
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+function formatPayload_updatePost(user, timestamp, channelID) {
+  let rotation = getLatestRotations();
+  let payload = {
+    channel: channelID,
+    ts: timestamp,
+    as_user: true,
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*Weekly Rotations Update* \n"
+        }
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `` 
+        }
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: ``
+        }
+      },
+      {
+        type: "divider",
+        block_id: "divider1"
+		  },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: ``
+        }
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: ``
+        }
+      }
+    ]
+  };
 
-async function rest() {
-  console.log('Taking a break...');
-  await sleep(5000);
-  console.log('Five seconds later');
+  let first = `*${rotation.first.day} (${rotation.first.date})*: *_${rotation.first.rotationStatus}_* \n`;
+  let second = `*${rotation.second.day} (${rotation.second.date})*: *_${rotation.second.rotationStatus}_* \n`;
+  let third = `*${rotation.third.day} (${rotation.third.date})*: *_${rotation.third.rotationStatus}_* \n`;
+  let fourth = `*${rotation.fourth.day} (${rotation.fourth.date})*: *_${rotation.fourth.rotationStatus}_* \n`;
+
+  if (rotation.first.prayerLead) {
+     first += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.first.prayerLead}* \n`;
+  }
+  if (rotation.first.vidDirector) {
+     first += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.first.vidDirector}* \n`;
+  }
+  if (rotation.first.proPres) {
+     first += `>${SHEET_ROWS.PROPRES}: *${rotation.first.proPres}* \n`;
+  }
+  if (rotation.first.cam1OP) {
+     first += `>${SHEET_ROWS.CAM1OP}: *${rotation.first.cam1OP}* \n`;
+  }
+  if (rotation.first.cam2OP) {
+     first += `>${SHEET_ROWS.CAM2OP}: *${rotation.first.cam2OP}* \n`;
+  }
+  if (rotation.first.cam3OP) {
+     first += `>${SHEET_ROWS.CAM3OP}: *${rotation.first.cam3OP}* \n`;
+  }
+  if (rotation.first.editor) {
+     first += `>${SHEET_ROWS.EDITOR}: *${rotation.first.editor}* \n`;
+  }
+  if (rotation.first.help) {
+     first += `>${SHEET_ROWS.HELP}: *${rotation.first.help}* \n`;
+  }
+  if (rotation.first.training) {
+     first += `>${SHEET_ROWS.TRAINING}: *${rotation.first.training}* \n`;
+  }
+  if (rotation.first.youthAssistants) {
+     first += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.friday.youthAssistants}* \n`;
+  }
+
+
+  if (rotation.second.prayerLead) {
+     second += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.second.prayerLead}* \n`;
+  }
+  if (rotation.second.vidDirector) {
+     second += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.second.vidDirector}* \n`;
+  }
+  if (rotation.second.proPres) {
+     second += `>${SHEET_ROWS.PROPRES}: *${rotation.second.proPres}* \n`;
+  }
+  if (rotation.second.cam1OP) {
+     second += `>${SHEET_ROWS.CAM1OP}: *${rotation.second.cam1OP}* \n`;
+  }
+  if (rotation.second.cam2OP) {
+     second += `>${SHEET_ROWS.CAM2OP}: *${rotation.second.cam2OP}* \n`;
+  }
+  if (rotation.second.cam3OP) {
+     second += `>${SHEET_ROWS.CAM3OP}: *${rotation.second.cam3OP}* \n`;
+  }
+  if (rotation.second.editor) {
+     second += `>${SHEET_ROWS.EDITOR}: *${rotation.second.editor}* \n`;
+  }
+  if (rotation.second.help) {
+     second += `>${SHEET_ROWS.HELP}: *${rotation.second.help}* \n`;
+  }
+  if (rotation.second.training) {
+     second += `>${SHEET_ROWS.TRAINING}: *${rotation.second.training}* \n`;
+  }
+  if (rotation.second.youthAssistants) {
+     second += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.second.youthAssistants}* \n`;
+  }
+
+  if (rotation.third.prayerLead.includes(user, 0)) {
+     third += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.third.prayerLead}* \n`;
+  }
+  if (rotation.third.vidDirector.includes(user, 0)) {
+     third += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.third.vidDirector}* \n`;
+  }
+  if (rotation.third.proPres.includes(user, 0)) {
+     third += `>${SHEET_ROWS.PROPRES}: *${rotation.third.proPres}* \n`;
+  }
+  if (rotation.third.cam1OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM1OP}: *${rotation.third.cam1OP}* \n`;
+  }
+  if (rotation.third.cam2OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM2OP}: *${rotation.third.cam2OP}* \n`;
+  }
+  if (rotation.third.cam3OP.includes(user, 0)) {
+     third += `>${SHEET_ROWS.CAM3OP}: *${rotation.thirdd.cam3OP}* \n`;
+  }
+  if (rotation.third.editor.includes(user, 0)) {
+     third += `>${SHEET_ROWS.EDITOR}: *${rotation.third.editor}* \n`;
+  }
+  if (rotation.third.help.includes(user, 0)) {
+     third += `>${SHEET_ROWS.HELP}: *${rotation.third.help}* \n`;
+  }
+  if (rotation.third.training.includes(user, 0)) {
+     third += `>${SHEET_ROWS.TRAINING}: *${rotation.third.training}* \n`;
+  }
+  if (rotation.third.youthAssistants.includes(user, 0)) {
+     third += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.third.youthAssistants}* \n`;
+  }
+
+  if (rotation.fourth.prayerLead.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.PRAYER_LEAD}: *${rotation.fourth.prayerLead}* \n`;
+  }
+  if (rotation.fourth.vidDirector.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.VID_DIRECTOR}: *${rotation.fourth.vidDirector}* \n`;
+  }
+  if (rotation.fourth.proPres.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.PROPRES}: *${rotation.fourth.proPres}* \n`;
+  }
+  if (rotation.fourth.cam1OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM1OP}: *${rotation.fourth.cam1OP}* \n`;
+  }
+  if (rotation.fourth.cam2OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM2OP}: *${rotation.fourth.cam2OP}* \n`;
+  }
+  if (rotation.fourth.cam3OP.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.CAM3OP}: *${rotation.fourth.cam3OP}* \n`;
+  }
+  if (rotation.fourth.editor.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.EDITOR}: *${rotation.fourth.editor}* \n`;
+  }
+  if (rotation.fourth.help.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.HELP}: *${rotation.fourth.help}* \n`;
+  }
+  if (rotation.fourth.training.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.TRAINING}: *${rotation.fourth.training}* \n`;
+  }
+  if (rotation.fourth.youthAssistants.includes(user, 0)) {
+     fourth += `>${SHEET_ROWS.YOUTH_ASSISTANTS}: *${rotation.fourth.youthAssistants}* \n`;
+  }
+  
+  Logger.log("payload timestamp: " + timestamp);
+  payload.blocks[1].text.text = first;
+  payload.blocks[2].text.text = second;
+  payload.blocks[4].text.text = third;
+  payload.blocks[5].text.text = fourth;
+
+  return payload;
 }
 
 function requestSlack(method, endpoint, params) {
   const base_url = "https://slack.com/api/";
   const headers = {
-    'Authorization': "<>",
+    'Authorization': "Bearer xoxb-3804154765057-3791572762146-PPnge7Boa8lqOsfAfFciPVhq",
     'Content-Type': 'application/json'
   };
   const options = {
